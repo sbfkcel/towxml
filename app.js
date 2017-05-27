b1f4dbf4db4fcd52823686bbceb469bcbd9817ac
sbfkcel <sbfkcel@163.com>
2017-05-27 18:30:28 +0800

    _

--- app.js
+++ app.js
@@ -6,8 +6,39 @@ const {marked,fs,path} = {
 
 let mdContent = fs.readFileSync('./doc.md').toString(),
 	mdHtml = marked(mdContent);
+
+//<hr>、<hr/>、<hr />、<br>、<br/>、<br />需要替换为view.xx
+let eautistic = ['hr','br'];
+
+//img需要替换为<image></image>
+let img = [];
+
+//需要替换为view.xx、
+let replace = [
+	'h1',
+	'h2',
+	'h3',
+	'h4',
+	'h5',
+	'h6',
+	'ul',
+	'ol',
+	'li',
+	'pre',
+	'code',
+	'blockquote',
+	'table',
+	'thead',
+	'th',
+	'tbody',
+	'tr',
+	'td',
+	'tfooter',
+	'p',
+	'a'
+];
 	
 
 
-console.log();
+console.log(mdHtml);
 

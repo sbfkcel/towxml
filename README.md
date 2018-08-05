138e236599415fe4496f938a9ce7573f181c819e
fan <fan@fandeMacBook-Pro.local>
2018-08-05 10:35:49 +0800

    _

---
+++ README.md
@@ -0,0 +1,232 @@
+<div align="center"><img width="240" src="https://cdn.rawgit.com/sbfkcel/towxml/78b0886d/logo.svg"/></div>
+
+# Towxml
+
+**Towxml** 是一个可将`HTML`、`Markdown`转为微信小程序`WXML`(WeiXin Markup Language)的渲染库。
+
+用于解决在微信小程序中`Markdown`、`HTML`不能直接渲染的问题。
+
+
+## 特色
+
+- 支持代码语法高亮
+- 支持emoji表情:wink:
+- 支持上标、下标、下划线、删除线、表格、视频、图片（几乎所有html元素）……
+- 支持typographer字符替换
+- 多主题动态支持
+- 极致的中文排版优化
+- Markdown TodoList
+- 支持事件绑定（这样允许自行扩展功能哟，例如：点击页面中的某个元素，更新当前页面内容等...）
+- 前后端支持
+
+
+## 截图
+
+以下截图即`demo`目录编译的效果
+
+![towxml](https://cdn.rawgit.com/sbfkcel/towxml/edc25e97/docs/demo.png)
+
+
+## 快速上手
+
+**1. 克隆TOWXML到小程序根目录**
+```bash
+git clone https://github.com/sbfkcel/towxml.git
+```
+
+**2. 在小程序`app.js`中引入库**
+```javascript
+//app.js
+const Towxml = require('/towxml/main');     //引入towxml库
+App({
+    onLaunch: function () {
+    },
+    towxml:new Towxml()                     //创建towxml对象，供小程序页面使用
+})
+```
+
+**3. 在小程序页面文件中引入模版**
+```html
+<!--pages/index.wxml-->
+
+<!--引入towxml模版入口文件，并使用模版-->
+<import src="/towxml/entry.wxml"/>
+<template is="entry" data="{{...article}}"/>
+```
+
+**4. 在小程序对应的js中请求数据**
+```javascript
+//pages/index.js
+
+const app = getApp();
+Page({
+    data: {
+        //article将用来存储towxml数据
+        article:{}
+    },
+    onLoad: function () {
+        const _ts = this;
+
+        //请求markdown文件，并转换为内容
+        wx.request({
+            url: 'http://xxx/doc.md',
+            header: {
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            success: (res) => {
+                //将markdown内容转换为towxml数据
+                let data = app.towxml.toJson(res.data,'markdown');
+
+                //设置文档显示主题，默认'light'
+                data.theme = 'dark';
+
+                //设置数据
+                _ts.setData({
+                    article: data
+                });
+            }
+        });
+    }
+})
+```
+
+**5. 引入对应的WXSS**
+```css
+/**pages/index.wxss**/
+
+/**基础风格样式**/
+@import '/towxml/style/main.wxss';
+
+
+/**如果页面有动态主题切换，则需要将使用到的样式全部引入**/
+
+/**主题配色（浅色样式）**/
+@import '/towxml/style/theme/light.wxss';
+
+/**主题配色（深色样式）**/
+@import '/towxml/style/theme/dark.wxss';
+```
+
+OK，大功告成~~
+
+
+## 事件绑定
+
+`towxml`支持以下事件绑定，可自行根据需要为内容添加绑定事件。（不支持`bindtap`等事件简写方法）
+
+```bash
+'bind:touchstart',
+'bind:touchmove',
+'bind:touchcancel',
+'bind:touchend',
+'bind:tap',
+'bind:longpress',
+'bind:longtap',
+'bind:transitionend',
+'bind:animationstart',
+'bind:animationiteration',
+'bind:animationend',
+'bind:touchforcechange'
+```
+
+```javascript
+Page({
+  data: {
+    isloading: true,
+    article: {}
+  },
+  onLoad: function () {
+    const _ts = this;
+
+    //将markdown内容转换为towxml数据，交将当前页面对象传入以创建默认事件对象
+    let articleData = app.towxml.toJson('<div name="button" id="button1">测试一个可点击的元素</div>', 'html', _ts);
+
+    //自定义事件，格式为`event_`+`绑定类型`+`_`+`事件类型`
+    //例如`bind:touchstart`则为：
+    this['event_bind_touchstart'] = (event)=>{
+        console.log(event.target.dataset._el);     // 打印出元素信息
+    };
+
+    // 给todoList添加监听事件
+    this['eventRun_todo_checkboxChange'] = (event)=>{
+        console.log(event.detail);                 // todoList checkbox发生change事件
+    };
+
+    //设置文章数据，并清除页面loading
+    _ts.setData({
+        article: articleData,
+        isloading: false
+    });
+  }
+})
+
+```
+
+## API
+如果为了追求极致的体验，建议将`markdown`、`html`转换为**towxml**数据的过程放在服务器上，在小程序中直接请求数据即可。
+
+**1. 依赖环境**
+
+需要 [Node.js](https://www.nodejs.org/) 环境。（已经安装请忽略）
+
+
+**2. 安装`towxml`**
+```bash
+npm install towxml
+```
+
+**3. 接口使用**
+```javascript
+const Towxml = require('towxml');
+const towxml = new Towxml();
+
+//Markdown转WXML
+let wxml = towxml.md2wxml('# Article title');
+
+//html转WXML
+let wxml = towxml.html2wxml('<h1>Article title</h1>');
+
+//Markdown转towxml数据
+let data = towxml.toJson('# Article title','markdown');
+
+//htm转towxml数据
+let data = towxml.toJson('# Article title');
+```
+
+## Demo示例
+1. 将`towxml/demo`添加为小程序工程
+2. 再克隆`towxml`到`demo`目录
+3. 使用小程序开发工具编译即可
+
+
+## 更新说明
+
+### 1.5.12
+
+- 增加Markdown TodoList 支持
+
+### 1.5.11
+
+- 调整事件绑定策略，剔除`capture-bind`绑定方式
+- 调整自定义事件时获取元素属性的方式
+    - `event.target.dataset._el`元素的所有属性
+- 剔除`data-url`、`data-src`、`data-alpha`、`data-id`、`data-name`的支持（因为元素数据传递方式更全，模版代码更少）
+
+
+### 1.5.7
+
+- 调整代码片段在模拟器上与真机不一至的问题
+
+
+## 1.5.6
+
+- 增加事件绑定方法，允许为元素添加自定义事件
+
+
+## Towxml使用交流群（微信）
+
+![微信群](https://user-images.githubusercontent.com/5469785/41187302-06d53916-6bd9-11e8-855e-94745c3301b4.png)
+
+
+## License
+MIT
---
+++ outTemplate.js
@@ -0,0 +1,173 @@
+class outwxml{
+    constructor(option){
+        const _ts = this;
+        _ts.config = {};
+
+        option = option || {};
+
+        for(let i in option){
+          _ts.config[i] = option[i];
+        };
+
+        let Towxml = require('./main');
+        _ts.m = {
+            fs:require('fs'),
+            path:require('path'),
+            towxml:new Towxml()
+        };
+    }
+    init(){
+        const _ts = this;
+
+        _ts.outtag();
+
+        let s = _ts.outwxml();
+        _ts.m.fs.writeFileSync('./renderTemplate.wxml',s);
+    }
+
+    //输出tag
+    outtag(id){
+        const _ts = this;
+        let s = '',
+            attr = _ts.outattr(),
+            wxmlTag = _ts.m.towxml.wxmlTag;
+        
+        wxmlTag.forEach((item,index)=>{
+	        let imgMode = ''
+	        if(item === 'image'){
+		        imgMode = 'mode="widthFix"';
+            };
+
+            // todo添加绑定事件
+            if(item === 'checkbox-group'){
+                attr += `bindchange="{{item.attr['bindchange']}}"`;
+            };
+            if(item === 'checkbox'){
+                attr += `value="{{item.attr['value']}}"`;
+            };
+            
+            s+= `<${item} wx:if="{{item.node === 'element' && item.tag === '${item}'}}" ${attr} ${imgMode}><block wx:for="{{item.child}}" wx:key="{{item}}"><template is="m${id}" data="{{item}}"/></block></${item}>`;
+            // s+= `
+            //         <${item} wx:if="{{item.node === 'element' && item.tag === '${item}'}}" ${attr} ${imgMode}>
+            //             <block wx:for="{{item.child}}" wx:key="{{item}}">
+            //                 <template is="m${id}" data="{{item}}"/>
+            //             </block>
+            //         </${item}>
+            // `;
+        });
+
+        return s;
+    }
+
+    //生成模版对应属性
+    outattr(){
+        const _ts = this;
+        
+        let s = '',
+            attr = [
+                'class','width','height','data','src','id','style','href','checked',
+                'bind:touchstart',
+                'bind:touchmove',
+                'bind:touchcancel',
+                'bind:touchend',
+                'bind:tap',
+                'bind:longpress',
+                'bind:longtap',
+                'bind:transitionend',
+                'bind:animationstart',
+                'bind:animationiteration',
+                'bind:animationend',
+                'bind:touchforcechange'
+            ];
+
+        s += `data-_el="{{item}}"`;
+        attr.forEach((item,index)=>{
+            
+            switch (item) {
+                case 'class':
+                    s += `${item}="{{item.attr.class}}"`;
+                break;
+                case 'href':
+                    s += `url="{{item.attr.${item}}}"`;
+                break;
+                default:
+                    let aItem = item.split(':');
+                    if(aItem.length > 1){
+                        s += `${item}='eventRun_${aItem[0]}_${aItem[1]}'`;
+                    }else{
+                        s += `${item}="{{item.attr['${item}']}}"`;
+                    };
+                break;
+            };                   
+        });
+        return s;
+    }
+
+    //wxml模版生成
+    outwxml(){
+        const _ts = this;
+        let s = '';
+
+        for (let i = 0, len = _ts.config.depth; i<len; i++){
+            let c = i < len - 1 ? i+1 : i;
+            let temp = `<template name="m${i}"><block wx:if="{{item.node === 'text'}}">{{item.text}}</block>${_ts.outtag(c)}</template>`;
+            // let temp = `<template name="m${i}">
+            //         <!--文字-->
+            //         <block wx:if="{{item.node === 'text'}}">
+            //             {{item.text}}
+            //         </block>
+                    
+            //         ${_ts.outtag(c)}
+                    
+            //     </template>
+            // `;
+            // let temp = `
+            //     <template name="m${i}">
+            //         <!--文字-->
+            //         <block wx:if="{{item.node === 'text'}}">
+            //             {{item.text}}
+            //         </block>
+                    
+            //         <!--视图-->
+            //         <view
+            //             wx:if="{{item.node === 'element' && item.tag === 'view'}}"
+            //             class="{{item.attr.class}}"
+            //             width="{{item.attr.width}}"
+            //             height="{{item.attr.height}}"
+            //             data="{{item.attr.data}}"
+            //             src="{{item.attr.src}}"
+            //             id="{{item.attr.id}}"
+            //             style="{{item.attr.style}}"
+            //         >
+            //             <block wx:for="{{item.child}}" wx:key="{{item}}">
+            //                 <template is="m${c}" data="{{item}}"/>
+            //             </block>
+            //         </view>
+
+            //         <!--按钮-->
+            //         <button
+            //             wx:if="{{item.tag === 'button'}}"
+            //             class="{{item.attr.class}}"
+            //             width="{{item.attr.width}}"
+            //             height="{{item.attr.height}}"
+            //             data="{{item.attr.data}}"
+            //             src="{{item.attr.src}}"
+            //             id="{{item.attr.id}}"
+            //             style="{{item.attr.style}}"
+            //         >
+            //             <block wx:for="{{item.child}}" wx:key="{{item}}">
+            //                 <template is="m${c}" data="{{item}}"/>
+            //             </block>
+            //         </button>
+                    
+            //     </template>
+            // `;
+
+            s+=temp;
+        };
+
+        return s;
+    }
+};
+
+new outwxml({depth:10}).init();
